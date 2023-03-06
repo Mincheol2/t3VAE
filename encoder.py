24ab344de5411ac427d9eb4766564b905790726a
Mincheol CHO <code1478@naver.com>
2023-03-06 17:54:06 +0900

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -1,97 +1,63 @@
-from encoder import Encoder
-from decoder import Decoder
-import argument
-import dataloader
 import torch
-import torchvision
+import torch.nn as nn
 import torch.optim as optim
-from util import *
-from skimage.metrics import structural_similarity as ssim
-from skimage.metrics import peak_signal_noise_ratio as psnr
-from sklearn.metrics import mean_squared_error as mse
-
+from torch.utils.data import DataLoader
+from torch.nn import functional as F
+import argument
+import random
+import math
+import numpy as np
+from loss import *
 args = argument.args
-class gammaAE():
-    def __init__(self, input_dim, DEVICE):
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim,DEVICE):
+        super(Encoder, self).__init__()
         self.input_dim = input_dim
-        self.DEVICE = DEVICE
-        self.encoder = Encoder(self.input_dim,DEVICE).to(DEVICE)
-        self.decoder = Decoder(self.input_dim).to(DEVICE)
-        self.opt = optim.Adam(list(self.encoder.parameters()) +
-                 list(self.decoder.parameters()), lr=args.lr, eps=1e-6, weight_decay=1e-5)
-
-
-        self.trainloader, self.testloader, self.sample_imgs = dataloader.select_dataloader()
-        self.sample_imgs = self.sample_imgs.to(DEVICE)
-    def train(self,epoch,writer):
-        self.encoder.train()
-        self.decoder.train()
-        total_loss = []
-        for batch_idx, (data, _) in enumerate(self.trainloader):
-            data = data.to(self.DEVICE)
-            self.opt.zero_grad()
-            z, mu, logvar = self.encoder(data)
-            reg_loss = self.encoder.loss(mu, logvar, self.input_dim)
-            recon_data = self.decoder(z)
-            recon_loss = self.decoder.loss(recon_data, data.view(-1,self.input_dim))
-            current_loss = reg_loss + recon_loss
-            current_loss.backward()
-
-            total_loss.append(current_loss.item())
+        self.device = DEVICE
+        self.fc1 = nn.Linear(self.input_dim, 400)
+        self.latent_mu = nn.Linear(400, args.zdim)
+        self.latent_var = nn.Linear(400, args.zdim)
+        
+    def reparameterize(self, mu, logvar):
+        
+        if args.nu == 0:
+            std = torch.exp(0.5 * logvar) # diagonal mat
+            eps = torch.randn_like(std) # Normal dist : eps ~ N(0, I)
+            return mu + std * eps
+        else:
+            '''
+                Sampling algorithm
+                Let nu_prime = nu + p_dim
+                1. Generate v ~ chiq(nu_prime) and eps ~ N(0, I), independently.
+                2. Caculate x = mu + std * eps / (sqrt(v/nu_prime)), where std = sqrt(nu/(nu_prime) * var)
+            '''
+            nu_prime = args.nu + args.zdim
+            MVN_dist = torch.distributions.MultivariateNormal(torch.zeros(args.zdim), torch.eye(args.zdim))
+            chi_dist = torch.distributions.chi2.Chi2(torch.tensor([nu_prime]))
             
-            self.opt.step()
+            # Student T dist : [B, z_dim]
+            eps = MVN_dist.sample(sample_shape=torch.tensor([mu.shape[0]])).to(self.device)
+            
+            std = torch.sqrt((args.nu / nu_prime) * torch.exp(0.5 * logvar))
+            v = chi_dist.sample().to(self.device)
+            return mu + std * eps * torch.sqrt(nu_prime / v)
 
-            if batch_idx % 200 == 0:
-                N = data.shape[0]
-                denom = len(self.trainloader.dataset)/args.batch_size
-                writer.add_scalar("Train/Reconstruction Error", recon_loss.item() / N, batch_idx + epoch * denom )
-                writer.add_scalar("Train/Regularizer", reg_loss.item() / N, batch_idx + epoch * denom )
-                writer.add_scalar("Train/Total Loss" , current_loss.item() / N, batch_idx + epoch * denom )
-        return reg_loss.item() / N, recon_loss.item() / N, current_loss.item() / N
+    def forward(self, x):
+        x = x.view(-1,self.input_dim)
+        x = F.relu(self.fc1(x))
+        mu = self.latent_mu(x)
+        logvar = self.latent_var(x)
+        z = self.reparameterize(mu, logvar)
 
-    def test(self,epoch,writer):
-        self.encoder.eval()
-        self.decoder.eval()
-        with torch.no_grad():
-            for batch_idx, (data, labels) in enumerate(self.testloader):
-                data = data.to(self.DEVICE)
-                z, mu, logvar = self.encoder(data)
-                            
-                reg_loss = self.encoder.loss(mu, logvar, self.input_dim)
-                recon_img = self.decoder(z)
-                data = data.view(-1,784)
-                recon_loss = self.decoder.loss(recon_img,data)
-                current_loss = reg_loss + recon_loss
+        return z, mu, logvar
 
-                ## Caculate SSIM, PSNR, RMSE ##
-                img1 = data.cpu().squeeze(dim=1).numpy()
-                img2 = recon_img.cpu().view_as(data).squeeze(dim=1).numpy()
-                ssim_test = 0
-                psnr_test = 0
-                rmse_test = 0
-                N = img1.shape[0]
-                for i in range(N):
-                    ssim_test += ssim(img1[i], img2[i])
-                    psnr_test += psnr(img1[i], img2[i])
-                    rmse_test += mse(img1[i], img2[i]) ** 0.5
-                ssim_test /= N
-                psnr_test /= N
-                rmse_test /= N
-                ## Add metrics to tensorboard ##
-                if batch_idx % 200 == 0:
-                    denom = len(self.testloader.dataset)/args.batch_size
-                    writer.add_scalar("Test/SSIM", ssim_test.item(), batch_idx + epoch * denom )
-                    writer.add_scalar("Test/PSNR", psnr_test.item(), batch_idx + epoch * denom )
-                    writer.add_scalar("Test/RMSE", rmse_test.item(), batch_idx + epoch * denom )
-                    
-                    writer.add_scalar("Test/Reconstruction Error", recon_loss.item() / N, batch_idx + epoch * denom )
-                    writer.add_scalar("Test/Regularizer", reg_loss.item() / N, batch_idx + epoch * denom )
-                    writer.add_scalar("Test/Total Loss" , current_loss.item() / N, batch_idx + epoch * denom)
-                
-            n = min(self.sample_imgs.shape[0], 32)
-            sample_z, _, _ = self.encoder(self.sample_imgs[:n])
-            test_imgs = self.decoder(sample_z)
-            comparison = torch.cat([self.sample_imgs[:n], test_imgs.view(n, 1, 28, 28)[:n]]) # (N, 1, 28, 28)
-            grid = torchvision.utils.make_grid(comparison.cpu())
-            writer.add_image("Test image - Above: Real data, below: reconstruction data", grid, epoch)
-        return reg_loss.item() / len(data), recon_loss.item() / len(data), current_loss.item() / len(data)
+    def loss(self, mu, logvar, input_dim):
+        if args.nu == 0:
+            # KL divergence
+            div_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        else:
+            # gammaAE regularizer
+            div_loss = gamma_regularizer(mu, logvar, input_dim)
+        
+        return div_loss * args.reg_weight
