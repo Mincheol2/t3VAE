853ed19fd1ae24d6d7289b7622861386b7be0c0e
Mincheol2 <code1478@naver.com>
2023-05-23 15:55:17 +0000

    update env

--- CelebA_experiments/exp_env/tiltedVAE.sh
+++ CelebA_experiments/exp_env/tiltedVAE.sh
@@ -1,2 +1,2 @@
 #! /bin/zsh
-python3 ../run.py --model TiltedVAE --dirname Test_tilt --batch_size 128 --tilt 40 --epoch 50 --lr 3e-4 --datapath ../
+python3 ../run.py --model TiltedVAE --dirname tilt_default --batch_size 128 --tilt 40 --epoch 50 --lr 3e-4 --datapath ../
